227b54e8c7bfc71492a178ae082368a8473b8dc2
nabdurahim-art <n.abdurahim@alustudent.com>
2026-05-20 05:34:51 -0700

    modified codes

---
+++ src/main.py
@@ -0,0 +1,72 @@
+import re
+import json
+
+
+# Open and read the raw text file
+with open("../input/raw-text.txt", "r") as file:
+    raw_text = file.read()
+
+
+# Find valid ALU email addresses only
+alu_email_regex = r'\b[A-Za-z0-9._%+-]+@(?:alueducation\.com|alumni\.alueducation\.com|si\.alueducation\.com)\b'
+
+
+# Find phone numbers
+phone_regex = r'(?:\+250|0)\s?\d{3}[\s-]?\d{3}[\s-]?\d{3}'
+
+
+# Find website links
+website_regex = r'https?://[^\s]+'
+
+
+# Find credit card numbers
+card_regex = r'\b(?:\d{4}[- ]?){3}\d{4}\b'
+
+
+# Extract valid data
+valid_emails = re.findall(alu_email_regex, raw_text)
+
+valid_phones = re.findall(phone_regex, raw_text)
+
+valid_websites = re.findall(website_regex, raw_text)
+
+
+# Hide sensitive card information
+safe_cards = [
+    "****-****-****-" + card.replace(" ", "-")[-4:]
+    for card in re.findall(card_regex, raw_text)
+]
+
+
+# Check for unsafe input
+if "<script>" in raw_text or "DROP TABLE" in raw_text:
+    print("\nSecurity Alert: Unsafe content detected in the text.")
+
+
+# Show results
+print("\nValid ALU Emails:")
+print(valid_emails)
+
+print("\nValid Phone Numbers:")
+print(valid_phones)
+
+print("\nValid Websites:")
+print(valid_websites)
+
+print("\nProtected Credit Cards:")
+print(safe_cards)
+
+
+# SAVE RESULTS TO JSON FILE
+
+output_data = {
+    "emails": valid_emails,
+    "phone_numbers": valid_phones,
+    "websites": valid_websites,
+    "credit_cards": safe_cards
+    }
+with open("../output/sample-output.json", "w") as json_file:
+    json.dump(output_data, json_file, indent=4)
+
+
+print("\nResults saved.")
